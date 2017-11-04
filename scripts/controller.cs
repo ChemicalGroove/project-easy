0a42b28ec9a8e9f8543de0f2cf3990cb416a819b
4SapH <skaterpotter@gmail.com>
2017-11-04 14:42:53 +0200

    Added turning character
    Scripts makes the player point at the mouse position.
    (press 's' to trigger on/off)

---
+++ scripts/controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+
+	bool stop;
+
+	public faceMouse faceMouse;
+
+	// Use this for initialization
+	void Start () {
+		stop = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.S)) {
+			stop = !stop;
+		}
+		if (!stop) faceMouse.face();
+	}
+}
---
+++ scripts/faceMouse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class faceMouse : MonoBehaviour {
+
+	Vector3 mousePos;
+
+	public void face () {
+    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+
+}
